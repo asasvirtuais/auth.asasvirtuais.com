10ab07e6ed5751ebe371feff39cc10a1eee99bce
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2023-06-26 23:17:06 -0300

    GURU PEMA SIDI HUM

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,3 +1,17 @@
-import { handleAuth } from '@auth0/nextjs-auth0'
+import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0'
 
-export default handleAuth()
+export default handleAuth({
+    async login(req, res) {
+        await handleLogin(req, res, {
+            returnTo: req.query.returnTo as string,
+            authorizationParams: {
+                scope: 'openid profile email' + req.query.scope ? ` ${req.query.scope as string}` : ''
+            }
+        })
+    },
+    async logout(req, res) {
+        await handleLogout(req, res, {
+            returnTo: req.query.returnTo as string,
+        })
+    }
+})
