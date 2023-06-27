c247d279c11ec6db8e1330b08ee8c966a6e41233
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2023-06-27 00:41:51 -0300

    OM AH HUM

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,13 +1,9 @@
 import { handleAuth, handleLogin, handleLogout, handleCallback } from '@auth0/nextjs-auth0'
 
 export default handleAuth({
-    async callback(req, res) {
-        await handleCallback(req, res, {
-            afterCallback(req, res, session, state) {
-                if ( req.query.returnTo )
-                    res.redirect(req.query.returnTo as string)
-                return session
-            }
+    async login(req, res) {
+        await handleLogin(req, res, {
+            returnTo: req.query.returnTo as string
         })
-    },
+    }
 })
