55790da1ee942e9b75e281656740ff12a9ccb69f
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2023-06-26 23:41:51 -0300

    OM AH HUM

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,6 +1,14 @@
-import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0'
+import { handleAuth, handleLogin, handleLogout, handleCallback } from '@auth0/nextjs-auth0'
 
 export default handleAuth({
+    async callback(req, res) {
+        await handleCallback(req, res, {
+            redirectUri: req.query.redirectUri as string,
+            authorizationParams: {
+                scope: (req.query.scope ? `${req.query.scope as string} ` : '') + 'openid profile email'
+            },
+        })
+    },
     async login(req, res) {
         await handleLogin(req, res, {
             returnTo: req.query.returnTo as string,
