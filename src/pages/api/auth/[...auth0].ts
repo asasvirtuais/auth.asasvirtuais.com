e0b0e8fa84d0a33c4c4c7676cae1e984ba847f00
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2023-07-10 15:12:39 -0300

    Debug

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,12 +1,3 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
 
-export default handleAuth({
-    async login( req, res ) {
-        await handleLogin( req, res, {
-            authorizationParams: {
-                connection: 'linkedin',
-                connection_scope: 'w_member_social'
-            }
-        } )
-    }
-})
+export default handleAuth()
