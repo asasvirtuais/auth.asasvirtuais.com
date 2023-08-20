10e6f7f20f5a92d57247bff9540351b42b50093c
Icaro Asas Virtuais <icaro.capobianco@gmail.com>
2023-08-20 12:24:54 -0300

    + LinkedAccount list item component

--- src/components/linked-accounts.tsx
+++ src/components/linked-accounts.tsx
@@ -1,25 +1,31 @@
-import { FormControl, FormLabel, Box, Skeleton } from '@chakra-ui/react'
+import { FormControl, Text, FormLabel, Box, Skeleton, HStack, Button } from '@chakra-ui/react'
 import { useUser } from '@/hooks/use-user'
+
+const LinkedAccount = ( identity: AsasAuth.Identity ) => {
+    return (
+        <HStack justify='space-between' align='center'>
+            <Text>
+                {identity.connection === 'google-oauth2' ? 'google' : identity.connection}
+            </Text>
+        </HStack>
+    )
+}
 
 export default function LinkedAccounts() {
     const { user, loading } = useUser()
     return (
         <>
             <FormControl>
-                <FormLabel>
-                    <b>Linked accounts</b>
-                    {loading ? (
-                            <Skeleton h={6} />
-                        ) : (
-                            <Box>
-                                {user?.identities?.filter( (i: any) => i.isSocial )?.map( (i: any) => (
-                                    <Box key={i.user_id}>
-                                        {i.connection === 'google-oauth2' ? 'google' : i.connection}
-                                    </Box>
-                                ))}
-                            </Box>
-                    )}
-                </FormLabel>
+                <b>Linked accounts</b>
+                {loading ? (
+                        <Skeleton h={6} />
+                    ) : (
+                        <Box>
+                            {user?.identities?.filter( (i: any) => i.isSocial )?.map( (i: any) => (
+                                <LinkedAccount key={i.user_id} {...i} />
+                            ))}
+                        </Box>
+                )}
             </FormControl>
         </>
     )
