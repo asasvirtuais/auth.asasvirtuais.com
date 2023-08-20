09db3c8160c7b59fe8f2f89c164869fbd258899d
Icaro Asas Virtuais <icaro.capobianco@gmail.com>
2023-08-20 12:24:18 -0300

    + tenantURL

--- src/util.ts
+++ src/util.ts
@@ -6,4 +6,6 @@ const isDev = env === 'development'
 const prodURL = 'https://auth.asasvirtuais.com'
 const devURL = 'https://auth.asasvirtuais.com'
 
+export const tenantURL = 'https://asasvirtuais.us.auth0.com'
+
 export const url = isDev ? devURL : prodURL
