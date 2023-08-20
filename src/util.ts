a388bb441d4834cd7484ee7693fa44d648ad1f55
Icaro Asas Virtuais <icaro.capobianco@gmail.com>
2023-08-20 13:08:02 -0300

    + accessible env variables

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,12 @@
-
 const env = process.env.AUTH_ENV
 
 const isDev = env === 'development'
 
-const prodURL = 'https://auth.asasvirtuais.com'
-const devURL = 'https://auth.asasvirtuais.com'
+const devURL = process.env.AUTH0_BASE_URL
+const prodURL = process.env.AUTH0_BASE_URL_PROD
 
-export const tenantURL = 'https://asasvirtuais.us.auth0.com'
+export const domain = process.env.AUTH0_DOMAIN as string
+export const tenantURL = process.env.AUTH0_ISSUER_BASE_URL as string
+export const clientId = process.env.AUTH0_CLIENT_ID as string
 
 export const url = isDev ? devURL : prodURL
